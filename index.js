3fcf5346be17a048700a472395b4cdf269a369cf
Domenico Ammendolia <dom1771@gmail.com>
2024-09-06 16:09:18 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,90 @@
+/*
+ * Simple JSON Api
+ *
+ */
+
+// Dependencies
+var http = require('http');
+var url = require('url');
+var StringDecoder = require('string_decoder').StringDecoder;
+var config = require('./config');
+
+
+ // Configure the server
+var server = http.createServer(function(req,res){
+
+  // Get basic objects
+  var parsedUrl = url.parse(req.url, true);
+  var path = parsedUrl.pathname;
+  var trimmedPath = path.replace(/^\/+|\/+$/g, '');
+  var queryStringObject = parsedUrl.query;
+  var method = req.method.toLowerCase();
+  var headers = req.headers;
+  var decoder = new StringDecoder('utf-8');
+  
+  // Payload
+  var buffer = '';
+  req.on('data', function(data) {
+      buffer += decoder.write(data);
+  });
+  req.on('end', function() {
+      // finalize Payload
+      buffer += decoder.end();
+
+      // Check the router for a matching path
+      var chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
+
+      // Construct the data object to send to the handler
+      var data = {
+          'trimmedPath' : trimmedPath,
+          'queryStringObject' : queryStringObject,
+          'method' : method,
+          'headers' : headers,
+          'payload' : payload
+        };
+
+        // Route the request to the handler specified in the router
+        chosenHandler(data,function(statusCode,payload){
+
+        // Use the status code returned from the handler, or set the default status code to 200
+        statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
+
+        // Use the payload returned from the handler, or set the default payload to an empty object
+        payload = typeof(payload) == 'object'? payload : {};
+
+        // Convert the payload to a string
+        var payloadString = JSON.stringify(payload);
+
+        // Return the response
+        res.setHeader('Content-Type', 'application/json');
+        res.writeHead(statusCode);
+        res.end(payloadString);
+        console.log("Returning this response: ",statusCode,payloadString);
+
+      });
+
+  });
+});
+
+// Start the server
+server.listen(config.port,function(){
+  console.log('The server is up and running on port '+config.port+' in '+config.envName+' mode.');
+});
+
+// Define all the handlers
+var handlers = {};
+
+// Hello
+handlers.hello = function(data,callback){
+    callback(200,{"output": "Welcome dear"});
+};
+
+// Not found handler
+handlers.notFound = function(data,callback){
+  callback(404);
+};
+
+// Define the request router
+var router = {
+  'hello' : handlers.hello
+};
